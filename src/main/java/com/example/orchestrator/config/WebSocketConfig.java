bdcb815580f21490ca9dc4e7407c271a9d76449d
august <ghrp8277@naver.com>
2024-06-30 06:07:07 +0900

    gradle update

--- src/main/java/com/example/orchestrator/config/WebSocketConfig.java
+++ src/main/java/com/example/orchestrator/config/WebSocketConfig.java
@@ -5,9 +5,6 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.HandshakeInterceptor;
-import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
-import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -22,28 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/v1/ws")
-            .setAllowedOrigins("*")
-            .addInterceptors(new HttpSessionHandshakeInterceptor(), new CustomHandshakeInterceptor())
-            .withSockJS();
-    }
-}
-
-class CustomHandshakeInterceptor implements HandshakeInterceptor {
-
-    @Override
-    public boolean beforeHandshake(org.springframework.http.server.ServerHttpRequest request,
-                                   org.springframework.http.server.ServerHttpResponse response,
-                                   org.springframework.web.socket.WebSocketHandler wsHandler,
-                                   Map<String, Object> attributes) throws Exception {
-        // 핸드셰이크 전에 처리할 로직 추가
-        return true;
-    }
-
-    @Override
-    public void afterHandshake(org.springframework.http.server.ServerHttpRequest request,
-                               org.springframework.http.server.ServerHttpResponse response,
-                               org.springframework.web.socket.WebSocketHandler wsHandler,
-                               Exception exception) {
-        // 핸드셰이크 후에 처리할 로직 추가
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 }
