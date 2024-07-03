d7c6bacdabc0a8c949c2f04ad6df065bd9b8f1a8
august <ghrp8277@naver.com>
2024-07-04 01:13:06 +0900

    stock add

--- src/main/java/com/example/orchestrator/service/StockService.java
+++ src/main/java/com/example/orchestrator/service/StockService.java
@@ -1,6 +1,7 @@
 package com.example.orchestrator.service;
 
-import com.example.grpc.StockServiceGrpc;
+import com.example.orchestrator.constants.NaverSymbolConstants;
+import com.example.orchestrator.service.grpc.StockGrpcService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import com.example.grpc.*;
@@ -8,9 +9,15 @@ import com.example.grpc.*;
 @Service
 @RequiredArgsConstructor
 public class StockService {
-    private final StockServiceGrpc stockServiceGrpc;
+    private final StockGrpcService stockGrpcService;
 
     public Response getStockData() {
+        GetStockDataRequest getStockDataRequest = GetStockDataRequest.newBuilder()
+                .setSymbol(NaverSymbolConstants.KOSPI.SK_HYNIX)
+                .setTimeframe(NaverSymbolConstants.TimeFrame.DAY)
+                .setCount(1250)
+                .build();
 
+        return stockGrpcService.getStockData(getStockDataRequest);
     }
 }
